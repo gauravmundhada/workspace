c50e19bbd4995ff4c316d3d4db3831c3d615e9d2
Iamneo <techadmin@iamneo.ai>
2023-11-10 08:39:23 +0000

    Backup Commit

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Header } from './Layout/header.component';
+import { Sidebar } from './Layout/sidebar.component';
 
 
 @NgModule({
