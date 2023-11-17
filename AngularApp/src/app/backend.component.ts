fd706841e6801257d0fad53f883c78ebaf68a504
Iamneo <techadmin@iamneo.ai>
2023-11-17 06:44:05 +0000

    Backup Commit

--- AngularApp/src/app/backend.component.ts
+++ AngularApp/src/app/backend.component.ts
@@ -1,5 +1,17 @@
 import { Component } from "@angular/core";
 
 @Component({
-    
+    selector:'crud',
+    templateUrl:'./backend.component.html' 
 })
+
+export class CRUD{
+    name:string="";
+    location:string="";
+    age:number = 0;
+
+    handleSave(){
+        
+    }
+
+}
