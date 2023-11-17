77004486edb9f9ac46993a3e8c49a9d4257cf18f
Iamneo <techadmin@iamneo.ai>
2023-11-17 06:15:07 +0000

    Backup Commit

---
+++ AngularApp/src/app/BackEndConnect/backend.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class ApiConnect{
+    constructor(private http:HttpClient){
+
+    }
+
+    //To create row
+    SaveData(data:any){
+        let responseData:any="";
+        let httpHeaders:HttpHeaders = new HttpHeaders({
+            Accept:'application/json'
+        })
+        this.http.post("",data,{headers:httpHeaders})
+        .subscribe(res=>{
+            responseData=res;
+        },
+        err=>{
+            responseData = err;
+        })
+        return responseData;
+    }
+
+    // To update row
+    UpdateData(id:number,data:any){
+
+    }
+
+    // To delete row
+    DeleteData(id:number){
+
+    }
+
+    // To fetch a list
+    GetData(){
+        
+    }
+}
