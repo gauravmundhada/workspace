3925c09c0ec8f7e3f7d704300019a0a36a3ebc95
Iamneo <techadmin@iamneo.ai>
2023-11-21 06:52:08 +0000

    Backup Commit

---
+++ API/Models/Employee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Employee
+    {
+        public string? Id { get;set; }
+        public string? Name { get;set; }
+        public string? Email { get;set; }
+        public int Phone { get;set; }
+        public int Salary { ge}
+
+    }
+}
