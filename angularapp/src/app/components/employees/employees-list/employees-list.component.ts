367f69b352c133e1d55b970c0e34df01648a3ec1
Iamneo <techadmin@iamneo.ai>
2023-11-21 06:07:25 +0000

    Backup Commit

--- angularapp/src/app/components/employees/employees-list/employees-list.component.ts
+++ angularapp/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/model/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
+  employees:Employee[]= [{
+    id: 'qaz123',
+    name: 'Gaurav',
+    email: 'gaurav@gmail.com',
+    phone: 8530919002,
+    salary: 578584,
+    department: 'Banking'
+  },
+  {
+    id: 'qaz124',
+    name: 'Sarang',
+    email: 'sarang@gmail.com',
+    phone: 8600712770,
+    salary: 57858400,
+    department: 'BusinessMan'    
+  },
+  {
+    id: 'qaz125',
+    name: 'Dinesh',
+    email: 'sarang@gmail.com',
+    phone: 8600712770,
+    salary: 57858400,
+    department: 'BusinessMan'  
+  }];
+  
   constructor() { }
 
   ngOnInit() {
