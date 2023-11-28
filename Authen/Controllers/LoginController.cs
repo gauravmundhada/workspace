6f31007c016237c33db92cd06f080c0f3114b1c9
Iamneo <techadmin@iamneo.ai>
2023-11-28 13:15:54 +0000

    Backup Commit

--- Authen/Controllers/LoginController.cs
+++ Authen/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Authen.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Authen.Controllers
 {
@@ -21,7 +23,22 @@ namespace Authen.Controllers
         //Autnenticate the users
         private Users AuthenticateUser(Users user)
         {
-            
+            Users _user = null;
+            if(user.Username == "admin" &&  user.Password == "12345")
+            {
+                _user = new Users{ Username = "Gaurav Mundhada"};
+            }
+            return _user;
+
+            private string GenerateToken(Users users)
+            {
+                var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+                // Create Token
+
+                var token = new JwtSecurityToken()
+            }
         }
     }
 }
