0715f03d21c5305168b44e2106b76dc545cf73d3
Iamneo <techadmin@iamneo.ai>
2023-11-28 13:05:51 +0000

    Backup Commit

--- Authen/Controllers/LoginController.cs
+++ Authen/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Authen.Models;
 
 namespace Authen.Controllers
 {
@@ -10,6 +11,17 @@ namespace Authen.Controllers
     [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
-        
+        //Injected Configuration
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        //Autnenticate the users
+        private Users AuthenticateUser(Users user)
+        {
+            
+        }
     }
 }
