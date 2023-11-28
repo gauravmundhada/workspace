8a3342b405a7a2824740b210b5ad3ec2eed16645
Iamneo <techadmin@iamneo.ai>
2023-11-28 12:30:54 +0000

    Backup Commit

---
+++ Authen/Models/Users.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authen.Models
+{
+    public class Users
+    {
+        public string Username { get;set; }
+        public string Password { get;set; }
+    }
+}
