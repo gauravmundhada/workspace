8ecadda8d2be83a572ce4aaf191cd886761c37ae
Iamneo <techadmin@iamneo.ai>
2023-11-29 12:10:20 +0000

    Backup Commit

--- FinalApi/Controllers/UserController.cs
+++ FinalApi/Controllers/UserController.cs
@@ -19,14 +19,32 @@ namespace FinalApi.Controllers
         }
 
         [HttpPost("authenticate")]
-        public async Task<IActionResult> Authenticate([FromBody User userObj])
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
         {
             if(userObj == null)
             {
                 return BadRequest();
             }
-                var user = await _authContext.Users.FirstOrDefaultAsync(x=>x.Username == userObj.Username);
-                
+                var user = _authContext.Users.FirstOrDefault(x=>x.Username == userObj.Username && x.Password == userObj.Password);
+                if(user==null)
+                    return NotFound(new { Message = "User Not Found!"});
+
+                return Ok(new{
+                    Message = "Login Success!"
+                });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody]User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            _authContext.SaveChangesAsync();
+            return Ok(new{
+                Message="User Registered Successfully!"
+            });
         }
 
     }
