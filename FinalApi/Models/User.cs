536cdde67cb81368d88034182611942a340d9036
Iamneo <techadmin@iamneo.ai>
2023-11-29 11:29:33 +0000

    Backup Commit

---
+++ FinalApi/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get;set; }
+        public string FirstName { get;set; }
+        public string LastName { get; set; }
+        public int MyProperty { get; set; }
+    }
+
+}
