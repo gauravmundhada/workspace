ce39474adf47e0a2efda9deb02e71dafa6980f63
Iamneo <techadmin@iamneo.ai>
2023-11-29 11:42:25 +0000

    Backup Commit

--- FinalApi/Models/User.cs
+++ FinalApi/Models/User.cs
@@ -11,7 +11,11 @@ namespace FinalApi.Models
         public int Id { get;set; }
         public string FirstName { get;set; }
         public string LastName { get; set; }
-        public int MyProperty { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
     }
 
 }
